d64f8c5f8715fddb9c4ebfd4dad1a73c441d28c6
HashirGJ8842 <hameedhashirniazi@gmail.com>
2020-03-30 02:56:38 +0530

    Correction

--- application.py
+++ application.py
@@ -6,33 +6,40 @@ app = Flask(__name__)
 
 @app.route('/api')
 def api():
-        res = requests.get('https://api.covid19api.com/summary')
-        data = dict(res.json())
-        LastUpdated = data["Date"]
-        data = data['Countries']
-        newDeath = {}
-        newCases = {}
-        newRecov = {}
-        totalDeath = {}
-        totalCases = {}
-        totalRecov = {}
-        for i in data:
-                newDeath[i["Country"]] = i['NewDeaths']
-                newCases[i["Country"]] = i['NewConfirmed']
-                newRecov[i["Country"]] = i['NewRecovered']
-                totalDeath[i["Country"]] = i['TotalDeaths']
-                totalCases[i["Country"]] = i['TotalConfirmed']
-                totalRecov[i["Country"]] = i['TotalRecovered']
-        object = {"Last Updated": LastUpdated, 'NewDeaths': newDeath, "NewCases": newCases, "NewRecovered": newRecov,
-         "TotalDeaths": totalDeath, "TotalCases": totalCases, "TotalRecovered": totalRecov}
-
-        return jsonify(object)
+    res = requests.get('https://api.covid19api.com/summary')
+    data = res.json()
+    LastUpdated = data["Date"]
+    data = data['Countries']
+    newDeath = {}
+    newCases = {}
+    newRecov = {}
+    totalDeath = {}
+    totalCases = {}
+    totalRecov = {}
+    for i in data:
+        if i["Country"] == "":
+                continue
+        newDeath[i["Country"]] = i['NewDeaths']
+        newCases[i["Country"]] = i['NewConfirmed']
+        newRecov[i["Country"]] = i['NewRecovered']
+        totalDeath[i["Country"]] = i['TotalDeaths']
+        totalCases[i["Country"]] = i['TotalConfirmed']
+        totalRecov[i["Country"]] = i['TotalRecovered']
+    return jsonify({
+        "Last Updated": LastUpdated,
+        'NewDeaths': newDeath,
+        "NewCases": newCases,
+        "NewRecovered": newRecov,
+        "TotalDeaths": totalDeath,
+        "TotalCases": totalCases,
+        "TotalRecovered": totalRecov
+    })
 
 
 @app.route('/')
 def index():
-        return render_template('index.html')
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-        app.run(debug=True)
+    app.run(debug=True)

90ce8693e72c88db165647dbc93743e2ab6a892e
HashirGJ8842 <hameedhashirniazi@gmail.com>
2020-03-30 02:13:39 +0530

    Added API call for the home page

--- application.py
+++ application.py
@@ -1,8 +1,35 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+
+@app.route('/api')
+def api():
+        res = requests.get('https://api.covid19api.com/summary')
+        data = dict(res.json())
+        LastUpdated = data["Date"]
+        data = data['Countries']
+        newDeath = {}
+        newCases = {}
+        newRecov = {}
+        totalDeath = {}
+        totalCases = {}
+        totalRecov = {}
+        for i in data:
+                newDeath[i["Country"]] = i['NewDeaths']
+                newCases[i["Country"]] = i['NewConfirmed']
+                newRecov[i["Country"]] = i['NewRecovered']
+                totalDeath[i["Country"]] = i['TotalDeaths']
+                totalCases[i["Country"]] = i['TotalConfirmed']
+                totalRecov[i["Country"]] = i['TotalRecovered']
+        return requests.jsonify["Last Updated":LastUpdated, 'NewDeaths':newDeath ,"NewCases":newCases, "NewRecovered":newRecov, "TotalDeaths":totalDeath, "TotalCases":totalCases, "TotalRecovered":totalRecov]
+
+
 @app.route('/')
-@app.route('/home')
 def index():
-        return render_template("index.html")
+        return render_template('index.html')
+
+
+if __name__ == "__main__":
+        app.run(debug=True)
--- static/index.js
+++ static/index.js
@@ -1,7 +1,7 @@
 // when the DOM is done loading run this
 document.addEventListener('DOMContentLoaded', () => {
     const request = new XMLHttpRequest()
-    request.open('GET','/home')
+    request.open('GET','/api')
     request.onload = () => {
         const data = JSON.parse(request.responseText)
         
